a1227e268141ebff131c13dc02c9f725d91a1e23
loretet <lorenzoluca.donati@gmail.com>
2026-08-07 16:14:24 +0200

    restructuring to separate transfer functions

---
+++ src/era5_abl/transfer_func.py
@@ -0,0 +1,95 @@
+import numpy as np
+import xarray as xr
+from .operations import interpolate_to_height
+
+def compute_epsilon(ds: xr.Dataset, location: str) ->  tuple[float, float]:
+    """ 
+    Computes ration between z (20 m) and z0 or zt from surface data.
+    """
+    # One can either select time-varying values (based on seasonality and land cover) from ERA5 or assume constant
+    # values depending on the location. Here, I choose to keep them constant (seemed suitable to the scope of this work).
+    # Bear in mind that epsilon only appears in logarithms in GL18 functions, meaning that the roughenss lengths variations 
+    # are compressed (e.g. 100% variation in z0 leads to 13% variation in log(z/z0) with z=20m)
+    if ds.attrs['Location'] == "Mace Head": # Roughly based on He et al. (2021). Reduced Sea-Surface Roughness Length at a Coastal Site. Atmosphere, 12(8):991. 
+        z0 = 0.005  # marine sector 
+        zt = 0.0005 # depends on wind direction. Might need additional filtering
+    elif ds.attrs['Location'] == "Cabauw": # Beljaars & Bosveld (1997). Cabauw data for the verification of land surface schemes. Journal of Climate, 10(6), 1194–1207.
+        z0 = 0.15
+        zt = z0*0.1
+    elif ds.attrs['Location'] == "Southern Great Plains": # Jacobs & Brutsaert. (1998). Momentum roughness and view-angle dependent heat roughness at a Southern Great Plains test-site. Journal of Hydrology, 211(1), 62-68.
+        z0 = 0.15  # quite variable during the year (crops)
+        zt = 0.003
+    elif ds.attrs['Location'] == "Summit Station": # Miller et al. (2017). Surface energy budget responses to radiative forcing at Summit, Greenland. The Cryosphere. 11(1), 497-516.
+        z0 = 0.0004
+        zt = 0.0001
+    else:
+        print("No location specified: assigning default roughness lengths values")
+        z0 = 0.1
+        zt = z0*0.2
+    return 20/z0, 20/zt
+
+def compute_zeta_GL18(ds: xr.Dataset, epsilon: float, epsilon_t: float) -> xr.Dataset:
+    """
+    Computes the atmospheric stability parameter zeta (z/L) from Eq. 21 of Gryanik & Lüpkes 2018 (GL18).
+    Considers z = 20 m. NB: ONLY valid for Ri >= 0! NB2: They used 10 m as reference height
+    """
+    ln_eps = np.log(epsilon)
+    ln_epst = np.log(epsilon_t)
+
+    A = (ln_eps**2) / ln_epst
+    B = ln_eps + 11.3
+    C = ln_epst + 6.4
+
+    coeff_linear = A 
+    coeff_nonlinear = (B**3.82) / (11.5 * (C**1.91))
+    bracket = (B**2) / C - A
+
+    # Extract (interpolate) Ri at z = 20 m AGL per timestep
+    Ri_20 = interpolate_to_height(ds, "Ri_g", None, 20.0)
+
+    Ri_20_pos = np.maximum(Ri_20, 0.0)  # Restrict to positive Ri (enutral/stable)
+    zeta = (coeff_linear * Ri_20_pos) + (
+        coeff_nonlinear * bracket * (Ri_20_pos**2.91)
+    )
+    # Assign to the starting dataset
+    ds = ds.assign(zeta_20=zeta)
+
+    return ds
+
+def compute_fm(ds: xr.Dataset, epsilon: float ) -> xr.DataArray:
+    """
+    Computes the momentum stability correction function f_m (Eq. 22 of Gryanik and Lüpkes)
+    Considers z = 20 m. NB: ONLY valid for Ri >= 0! NB2: They used 10 m as reference height
+    """
+    zeta_20_pos = np.maximum(ds.zeta_20, 0.0) # Restrict to positive Richardson (neutral/stable)
+    x = np.cbrt(1.0 + zeta_20_pos)
+    ln_eps = np.log(epsilon)
+
+    num_log = (x + 0.67) ** 2
+    den_log = (x**2) - (0.67 * x) + 0.45
+
+    term1 = 10.29 - (19.5 * x)
+    term2 = 2.18 * np.log(num_log / den_log)
+    term3 = 7.54 * np.arctan((1.725 * x) - 0.58)
+
+    bracket = (term1 + term2 + term3) / ln_eps
+    fm = (1.0 - bracket) ** (-2.0)
+
+    return fm
+
+def compute_fh(fm: xr.DataArray, ds: xr.Dataset, epsilon_t: float) -> xr.DataArray:
+    """
+    Computes the momentum stability correction function f_m (Eq. 22 of Gryanik and Lüpkes)
+    Considers z = 10 m. NB: ONLY valid for Ri >= 0!
+    """
+    ln_epst = np.log(epsilon_t)
+
+    zeta_20_pos = np.maximum(ds.zeta_20, 0.0) # Restrict to positive values (neutral/stable)
+    term1 = 2.16
+    term2 = 2.5 * np.log(1.0 + (3.0 * zeta_20_pos) + (zeta_20_pos**2))
+    term3 = 1.12 * np.log((zeta_20_pos + 0.38) / (zeta_20_pos + 2.62))
+
+    bracket = (term1 - term2 + term3) / ln_epst
+    fh = np.sqrt(fm) * ((1.0 - bracket) ** (-1.0))
+
+    return fh
