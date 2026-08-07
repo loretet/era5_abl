ba79987426dd56a768746f2ac7bfe93d1c25c8ad
loretet <lorenzoluca.donati@gmail.com>
2026-08-07 16:41:08 +0200

    created stability file and moved stuff around

--- src/era5_abl/__init__.py
+++ src/era5_abl/__init__.py
@@ -7,14 +7,17 @@ from .preprocessing import (
 )
 
 from .operations import (
-    compute_grad_Ri_z,
-    compute_bulk_Ri,
     compute_BLH_from_Ri_b,
     compute_wind_dir,
     interpolate_to_height,
     compute_PDF,
-    compute_difference_surface_top_ABL,
     compare_diagnosed_and_era5_blh,
+)
+
+from .stability import(
+    compute_grad_Ri_z,
+    compute_bulk_Ri,
+    compute_difference_surface_top_ABL,
 )
 
 from .filters import (
