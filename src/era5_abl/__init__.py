08b9560755ddc008a382cf7146603dd02f0a7a8a
loretet <lorenzoluca.donati@gmail.com>
2026-08-07 16:31:43 +0200

    created __init__

---
+++ src/era5_abl/__init__.py
@@ -0,0 +1,41 @@
+"""
+ERA5 atmospheric boundary-layer diagnostics and filtering.
+"""
+
+from .preprocessing import (
+    prepare_dataset,
+)
+
+from .operations import (
+    compute_grad_Ri_z,
+    compute_bulk_Ri,
+    compute_BLH_from_Ri_b,
+    compute_wind_dir,
+    interpolate_to_height,
+    compute_PDF,
+    compute_difference_surface_top_ABL,
+    compare_diagnosed_and_era5_blh,
+)
+
+from .filters import (
+    filter_clouds,
+    filter_stability, 
+    filter_wind_dir,
+)
+
+__all__ = [
+    "prepare_dataset",
+    "compute_grad_Ri_z",
+    "compute_bulk_Ri",
+    "compute_BLH_from_Ri_b",
+    "compare_diagnosed_and_era5_blh",
+    "filter_clouds",
+    "filter_stability",
+    "filter_wind_dir",
+    "compute_wind_dir",
+    "interpolate_to_height",
+    "compute_PDF",
+    "compute_difference_surface_top_ABL",
+]
+
+__version__ = "0.1.0"
